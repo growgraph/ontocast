10eaf725952c4effe9014531336382640d9b3e0c
Alexander Belikov <abelikov@gmail.com>
2025-10-22 12:08:37 +0200

    isolated docling

--- ontocast/tool/converter.py
+++ ontocast/tool/converter.py
@@ -4,15 +4,18 @@ This module provides functionality for converting various document formats
 into structured data that can be processed by the OntoCast system.
 """
 
+import logging
+import pathlib
 from io import BytesIO
 from typing import Any, Union
 
 from docling.datamodel.base_models import (
     DocumentStream,
 )
-from docling.document_converter import DocumentConverter
 
 from .onto import Tool
+
+logger = logging.getLogger(__name__)
 
 
 class ConverterTool(Tool):
@@ -36,21 +39,34 @@ class ConverterTool(Tool):
         Args:
             **kwargs: Additional keyword arguments passed to the parent class.
         """
-        super().__init__(**kwargs)
-        self._converter = DocumentConverter()
+        try:
+            from docling.document_converter import DocumentConverter
 
-    def __call__(self, file_input: Union[bytes, str]) -> dict[str, Any]:
+            super().__init__(**kwargs)
+            self._converter: DocumentConverter | None = DocumentConverter()
+        except ImportError as e:
+            logger.error(f"Could not import DocumentConverter: {e}")
+
+    def __call__(self, file_input: Union[bytes, str, pathlib.Path]) -> dict[str, Any]:
         """Convert a document to structured data.
 
         Args:
-            file_input: The input file as either a BytesIO object or file path.
+            file_input: The input file as either bytes, string, or pathlib.Path.
 
         Returns:
             dict[str, Any]: The converted document data.
         """
         if isinstance(file_input, bytes):
             ds = DocumentStream(name="doc", stream=BytesIO(file_input))
+            if self._converter is None:
+                raise ImportError(f"Could not import DocumentConverter: {file_input}")
             result = self._converter.convert(ds)
+            doc = result.document.export_to_markdown()
+            return {"text": doc}
+        elif isinstance(file_input, pathlib.Path):
+            if self._converter is None:
+                raise ImportError(f"Could not import DocumentConverter: {file_input}")
+            result = self._converter.convert(file_input)
             doc = result.document.export_to_markdown()
             return {"text": doc}
         else:
